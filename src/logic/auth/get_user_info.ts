938c1e864c65aea988576c9c0e1267a149dc5d1d
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-15 13:03:17 +0800

    feat(logic/auth/get_user_info): done

--- src/logic/auth/get_user_info.ts
+++ src/logic/auth/get_user_info.ts
@@ -1,11 +1,24 @@
+import { ValidationError } from "myzod";
 import CSCAuth from "../../auth";
-import { AuthTokenResponse } from "../../types/auth/resp_auth_token";
+import BuildUri from "../../request/build_uri";
+import GetJsonAuth from "../../request/json/get_auth";
+import { AuthUserResponse, AuthUserResponseSchema } from "../../types/auth/resp_auth_user";
+import { StdErrOrNull, WouldFail } from "../../types/error/std_error";
 
 /**
  * Get the info of @param auth.
- * 
- * @returns Is it success?
  */
-export default function GetUserInfo(auth: CSCAuth): boolean {
-    throw new Error("not implemented");
+export default async function GetUserInfo(auth: CSCAuth): Promise<WouldFail<AuthUserResponse>> {
+    const rRes = await GetJsonAuth(BuildUri("/auth/user"), auth);
+
+    if (rRes && rRes.ok) {
+        const response = await rRes.json();
+
+        const successResp = AuthUserResponseSchema.try(response);
+        if (!(successResp instanceof ValidationError)) return successResp;
+
+        return StdErrOrNull(response);
+    }
+
+    return null;
 }
