13fe611c65feababd79d64eb360c7aa9f344aa3a
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-15 12:21:39 +0800

    feat(logic/common/is_api_working): work now

--- src/logic/common/is_api_working.ts
+++ src/logic/common/is_api_working.ts
@@ -1,3 +1,12 @@
-export default function IsApiWorking(): boolean {
-    throw new Error("not implemented");
+import { ValidationError } from "myzod";
+import BuildUri from "../../request/build_uri";
+import GetJson from "../../request/json/get";
+import { IsApiWorkingResponseSchema } from "../../types/common/resp_is_api_working";
+
+export default async function IsApiWorking(): Promise<boolean> {
+    const rRes: unknown = await GetJson(BuildUri("/"));
+    const res = IsApiWorkingResponseSchema.try(rRes);
+
+    if (res instanceof ValidationError) return false;
+    return res.success;
 }
