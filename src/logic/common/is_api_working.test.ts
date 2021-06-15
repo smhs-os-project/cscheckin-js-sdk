2a82a0f24045b9c3927b488fa6c3becb98e7134a
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-16 02:04:53 +0800

    feat(logic/common/is_api_working/test): allow to be used on Node.js

--- src/logic/common/is_api_working.test.ts
+++ src/logic/common/is_api_working.test.ts
@@ -1,7 +1,4 @@
 import IsApiWorking from "./is_api_working"
-import fetch from "node-fetch";
-
-globalThis.fetch = jest.fn(fetch) as any;
 
 test("API should be working", async () => {
     expect(await IsApiWorking()).toBe(true);
