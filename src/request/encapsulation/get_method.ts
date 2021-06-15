4fe8feff995172e22675515b8c68b079100c6974
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-15 22:54:26 +0800

    fix(request/encap/get_method): should not receive 'auth'

--- src/request/encapsulation/get_method.ts
+++ src/request/encapsulation/get_method.ts
@@ -1,12 +1,11 @@
-import CSCAuth from "../../auth";
 import BuildUri from "../build_uri";
-import GetJsonAuth from "../json/get_auth";
 import { AnyType } from "myzod/libs/types";
 import ResponseChecker from "./response_checker";
+import GetJson from "../json/get";
 
 export default async function GetMethod<T extends AnyType>
-    (method: string, auth: CSCAuth, schema: T) {
-    const res = await GetJsonAuth(BuildUri(method), auth);
+    (method: string, schema: T) {
+    const res = await GetJson(BuildUri(method));
 
     return ResponseChecker(res, schema);
 }
