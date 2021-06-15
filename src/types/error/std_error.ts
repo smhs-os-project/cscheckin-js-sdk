6094b32c72110cf3308b716f1461d796ac669df6
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-15 13:04:25 +0800

    feat(types/error/std_error): add StdErrOrNull

--- src/types/error/std_error.ts
+++ src/types/error/std_error.ts
@@ -1,4 +1,4 @@
-import myzod, {Infer} from "myzod";
+import myzod, {Infer, ValidationError} from "myzod";
 
 export const StandardErrorResponseSchema = myzod.object({
     message: myzod.string(),
@@ -6,3 +6,10 @@ export const StandardErrorResponseSchema = myzod.object({
 
 export type StandardErrorResponse = Infer<typeof StandardErrorResponseSchema>;
 export type WouldFail<T> = T | StandardErrorResponse | null;
+
+export const StdErrOrNull = (src: unknown) => {
+    const checked = StandardErrorResponseSchema.try(src);
+
+    if (checked instanceof ValidationError) return null;
+    return checked;
+}
