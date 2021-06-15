8f25c077b3e98cf90b27954cf6bb4764904680ad
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-15 11:08:11 +0800

    refactor(logic/auth/get_access_token): update stub

--- src/logic/auth/get_access_token.ts
+++ src/logic/auth/get_access_token.ts
@@ -1,3 +1,11 @@
-export default function GetAccessToken(gIdToken: string, gAccessToken: string): string {
+// import BuildUri from "../../request/build_uri";
+import { AuthTokenRequest } from "../../types/auth/req_auth_token";
+import { AuthTokenResponse } from "../../types/auth/resp_auth_token";
+import { WouldFail } from "../../types/error/std_error";
+
+/**
+ * Get the access token.
+ */
+export default async function GetAccessToken(request: AuthTokenRequest): Promise<WouldFail<AuthTokenResponse>> {
     throw new Error("not implemented");
 }
