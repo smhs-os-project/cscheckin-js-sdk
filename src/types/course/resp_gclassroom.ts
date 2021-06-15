3876bab9b3279ee38fce19fe177620cc2744d4e2
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-16 01:25:10 +0800

    feat(types/course/resp_gclassroom): done

---
+++ src/types/course/resp_gclassroom.ts
@@ -0,0 +1,10 @@
+import myzod, { Infer } from "myzod";
+
+export const GClassroomResponseSchema = myzod.object({
+    google_classroom_id: myzod.string(),
+    name: myzod.string(),
+});
+export const GClassroomListResponseSchema = myzod.array(GClassroomResponseSchema);
+
+export type GClassroomResponse = Infer<typeof GClassroomResponseSchema>;
+export type GClassroomListResponse = Infer<typeof GClassroomListResponseSchema>;
