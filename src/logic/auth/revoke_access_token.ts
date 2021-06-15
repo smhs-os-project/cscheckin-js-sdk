084afcf50f53ceb13092962787ffe42361660148
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-15 11:05:49 +0800

    feat(logic/auth/revoke_access_token): add stub

---
+++ src/logic/auth/revoke_access_token.ts
@@ -0,0 +1,11 @@
+import CSCAuth from "../../auth";
+import { AuthTokenResponse } from "../../types/auth/resp_auth_token";
+
+/**
+ * Revoke the access token
+ * 
+ * @returns Is it success?
+ */
+export default function RevokeAccessToken(auth: CSCAuth): boolean {
+    throw new Error("not implemented");
+}
