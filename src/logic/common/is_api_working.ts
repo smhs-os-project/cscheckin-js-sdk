4ed48c28b5d0433350335923c5ea4bbd54fd7883
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-15 22:53:54 +0800

    refactor(logic/auth/get_access_token): GetMethod

--- src/logic/common/is_api_working.ts
+++ src/logic/common/is_api_working.ts
@@ -1,15 +1,7 @@
-import { ValidationError } from "myzod";
-import BuildUri from "../../request/build_uri";
-import GetJson from "../../request/json/get";
-import { IsApiWorkingResponseSchema } from "../../types/common/resp_is_api_working";
+import GetMethod from "../../request/encapsulation/get_method";
+import { IsApiWorkingResponse, IsApiWorkingResponseSchema } from "../../types/common/resp_is_api_working";
 
 export default async function IsApiWorking(): Promise<boolean> {
-    const rRes = await GetJson(BuildUri("/"));
-
-    if (rRes.ok) {
-        const res = IsApiWorkingResponseSchema.try(await rRes.json());
-        if (!(res instanceof ValidationError)) return res.success;
-    }
-
-    return false;
+    const res = await GetMethod("/", IsApiWorkingResponseSchema);
+    return (res as IsApiWorkingResponse)?.success ?? false;
 }
