b576b19a486c78edfc39157a6d606693991045f3
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-15 22:56:18 +0800

    refactor(logic/org/get_org_list): GetMethod

--- src/logic/org/get_org_list.ts
+++ src/logic/org/get_org_list.ts
@@ -1,24 +1,10 @@
-import { ValidationError } from "myzod";
-import BuildUri from "../../request/build_uri";
-import GetJson from "../../request/json/get";
-import { StdErrOrNull, WouldFail } from "../../types/error/std_error";
-import { OrgInfoListResponse, OrgInfoListResponseSchema } from "../../types/org_info/resp_org_info";
+import GetMethod from "../../request/encapsulation/get_method";
+import { OrgInfoListResponseSchema } from "../../types/org_info/resp_org_info";
 
 /**
  * Get the organization list as well as
  * its Chinese name and client ID.
  */
-export default async function GetOrganizationList(): Promise<WouldFail<OrgInfoListResponse>> {
-    const rRes = await GetJson(BuildUri(`/info`));
-
-    if (rRes.ok) {
-        const response = await rRes.json();
-
-        const successResp = OrgInfoListResponseSchema.try(response);
-        if (!(successResp instanceof ValidationError)) return successResp;
-
-        return StdErrOrNull(response);
-    }
-
-    return null;
+export default async function GetOrganizationList() {
+    return GetMethod("/info", OrgInfoListResponseSchema);
 }
