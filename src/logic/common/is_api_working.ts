6378432a5e8eed193017b7a22533b8611a6f1885
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-15 12:30:31 +0800

    fix(logic/common/is_api_working): GetJson -> Response

--- src/logic/common/is_api_working.ts
+++ src/logic/common/is_api_working.ts
@@ -4,9 +4,12 @@ import GetJson from "../../request/json/get";
 import { IsApiWorkingResponseSchema } from "../../types/common/resp_is_api_working";
 
 export default async function IsApiWorking(): Promise<boolean> {
-    const rRes: unknown = await GetJson(BuildUri("/"));
-    const res = IsApiWorkingResponseSchema.try(rRes);
+    const rRes = await GetJson(BuildUri("/"));
 
-    if (res instanceof ValidationError) return false;
-    return res.success;
+    if (rRes.ok) {
+        const res = IsApiWorkingResponseSchema.try(await rRes.json());
+        if (!(res instanceof ValidationError)) return res.success;
+    }
+
+    return false;
 }
