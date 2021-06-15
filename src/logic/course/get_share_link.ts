1392a9169ded1867975e1715cc0bc46200f48d2f
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-16 01:45:38 +0800

    feat(src/logic/course/get_share_link): done

---
+++ src/logic/course/get_share_link.ts
@@ -0,0 +1,10 @@
+import CSCAuth from "../../auth";
+import GetAuthMethod from "../../request/encapsulation/get_auth_method";
+import { ShareResponseSchema } from "../../types/course/resp_share";
+
+/**
+ * Get the share link.
+ */
+export default async function GetShareLink(courseId: string, auth: CSCAuth) {
+    return GetAuthMethod(`/course/share/${courseId}`, auth, ShareResponseSchema);
+}
