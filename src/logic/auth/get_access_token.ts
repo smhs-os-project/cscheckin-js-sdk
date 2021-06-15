9a4ec93a62348e24faa8f7db8e9a796a39d81a40
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-16 01:43:41 +0800

    feat(logic/auth/get_access_token): Organization

--- src/logic/auth/get_access_token.ts
+++ src/logic/auth/get_access_token.ts
@@ -1,10 +1,12 @@
 import PostMethod from "../../request/encapsulation/post_method";
-import { AuthTokenRequest } from "../../types/auth/req_auth_token";
+import { AuthTokenRequest, Organization } from "../../types/auth/req_auth_token";
 import { AuthTokenResponseSchema } from "../../types/auth/resp_auth_token";
 
 /**
  * Get the access token.
+ * 
+ * @organization The organization issuing the id_token and access_token
  */
-export default async function GetAccessToken(organization: string, request: AuthTokenRequest) {
+export default async function GetAccessToken(organization: Organization, request: AuthTokenRequest) {
     return PostMethod(`/auth/token/${organization}`, request, AuthTokenResponseSchema);
 }
--- src/types/auth/req_auth_token.ts
+++ src/types/auth/req_auth_token.ts
@@ -1,5 +1,10 @@
 import myzod, { Infer } from "myzod";
 
+export enum Organization {
+    SMHS = "smhs",
+    FSSH = "fssh"
+}
+
 export const AuthTokenRequestSchema = myzod.object({
     id_token: myzod.string(),
     access_token: myzod.string(),
