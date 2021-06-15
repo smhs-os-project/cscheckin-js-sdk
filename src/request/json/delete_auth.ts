b88502e198198740b62b43a60f99cfc33310a9c9
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-15 22:28:59 +0800

    feat(request/json/delete_auth): done

---
+++ src/request/json/delete_auth.ts
@@ -0,0 +1,16 @@
+import CSCAuth from "../../auth";
+import BaseGetJson from "./get_base";
+
+export default async function DeleteJsonAuth(url: RequestInfo, auth: CSCAuth): Promise<Response | null> {
+    const authentication = await auth.getAuthenticationHeader();
+
+    if (authentication)
+        return BaseGetJson(url, {
+            method: "DELETE",
+            headers: {
+                "Authorization": authentication,
+            },
+        });
+    
+    return null;
+}
