4b08daf6f36adba521dd76558b672f80704bf749
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-15 13:03:02 +0800

    feat(logic/auth/get_access_token): done

--- src/logic/auth/get_access_token.ts
+++ src/logic/auth/get_access_token.ts
@@ -1,11 +1,25 @@
 // import BuildUri from "../../request/build_uri";
+import { ValidationError } from "myzod";
+import BuildUri from "../../request/build_uri";
+import PostJson from "../../request/json/post";
 import { AuthTokenRequest } from "../../types/auth/req_auth_token";
-import { AuthTokenResponse } from "../../types/auth/resp_auth_token";
-import { WouldFail } from "../../types/error/std_error";
+import { AuthTokenResponse, AuthTokenResponseSchema } from "../../types/auth/resp_auth_token";
+import { StdErrOrNull, WouldFail } from "../../types/error/std_error";
 
 /**
  * Get the access token.
  */
-export default async function GetAccessToken(request: AuthTokenRequest): Promise<WouldFail<AuthTokenResponse>> {
-    throw new Error("not implemented");
+export default async function GetAccessToken(organization: string, request: AuthTokenRequest): Promise<WouldFail<AuthTokenResponse>> {
+    const rRes = await PostJson(BuildUri(`/auth/token/${organization}`), request);
+
+    if (rRes.ok) {
+        const response = await rRes.json();
+
+        const successResp = AuthTokenResponseSchema.try(response);
+        if (!(successResp instanceof ValidationError)) return successResp;
+
+        return StdErrOrNull(response);
+    }
+
+    return null;
 }
