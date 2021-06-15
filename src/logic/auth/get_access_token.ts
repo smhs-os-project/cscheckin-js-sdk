68adde10357f28228fe9a5aeae95266b837ac707
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-15 22:53:09 +0800

    refactor(logic/auth/get_access_token): PostMethod

--- src/logic/auth/get_access_token.ts
+++ src/logic/auth/get_access_token.ts
@@ -1,25 +1,10 @@
-// import BuildUri from "../../request/build_uri";
-import { ValidationError } from "myzod";
-import BuildUri from "../../request/build_uri";
-import PostJson from "../../request/json/post";
+import PostMethod from "../../request/encapsulation/post_method";
 import { AuthTokenRequest } from "../../types/auth/req_auth_token";
-import { AuthTokenResponse, AuthTokenResponseSchema } from "../../types/auth/resp_auth_token";
-import { StdErrOrNull, WouldFail } from "../../types/error/std_error";
+import { AuthTokenResponseSchema } from "../../types/auth/resp_auth_token";
 
 /**
  * Get the access token.
  */
-export default async function GetAccessToken(organization: string, request: AuthTokenRequest): Promise<WouldFail<AuthTokenResponse>> {
-    const rRes = await PostJson(BuildUri(`/auth/token/${organization}`), request);
-
-    if (rRes.ok) {
-        const response = await rRes.json();
-
-        const successResp = AuthTokenResponseSchema.try(response);
-        if (!(successResp instanceof ValidationError)) return successResp;
-
-        return StdErrOrNull(response);
-    }
-
-    return null;
+export default async function GetAccessToken(organization: string, request: AuthTokenRequest) {
+    return PostMethod(`/auth/token/${organization}`, request, AuthTokenResponseSchema);
 }
