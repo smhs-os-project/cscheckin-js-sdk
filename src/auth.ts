4d5141f4b29d0d4c9321c6b9799a9ed3e1f1581b
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-15 11:05:09 +0800

    feat(auth): done

--- src/auth.ts
+++ src/auth.ts
@@ -1,15 +1,35 @@
-import GetAccessToken from "./logic/get_access_token";
+import { ValidationError } from "myzod";
+import GetAccessToken from "./logic/auth/get_access_token";
+import { AuthTokenResponse, AuthTokenResponseSchema } from "./types/auth/resp_auth_token";
 
 export default class CSCAuth {
-    private accessToken: string | undefined;
+    private accessData: AuthTokenResponse | null = null;
 
     constructor(private gIdToken: string, private gAccessToken: string) {}
 
-    get AccessToken() {
+    async getAccessData(): Promise<AuthTokenResponse | null> {
         // get lazily
-        if (!this.accessToken) {
-            this.accessToken = GetAccessToken(this.gIdToken, this.gAccessToken);
+        if (!this.accessData) {
+            const accessData = await GetAccessToken({
+                id_token: this.gIdToken,
+                access_token: this.gAccessToken
+            });
+            const resp = AuthTokenResponseSchema.try(accessData);
+
+            if (resp instanceof ValidationError) {
+                console.error("error: accessData is not a valid data.");
+                console.log(accessData);
+            } else {
+                this.accessData = resp;
+            }
         }
-        return this.accessToken;
+
+        return null;
+    }
+
+    async getAuthenticationHeader(): Promise<string | null> {
+        const accessData = await this.getAccessData();
+        if (accessData) return `Bearer ${accessData.access_token}`;
+        return null;
     }
 }
