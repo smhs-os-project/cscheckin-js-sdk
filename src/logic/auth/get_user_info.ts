ca69eea7712215b30c44a693065ad6a45180c7a3
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-15 22:53:21 +0800

    refactor(logic/auth/get_user_info): GetAuthMethod

--- src/logic/auth/get_user_info.ts
+++ src/logic/auth/get_user_info.ts
@@ -1,24 +1,10 @@
-import { ValidationError } from "myzod";
 import CSCAuth from "../../auth";
-import BuildUri from "../../request/build_uri";
-import GetJsonAuth from "../../request/json/get_auth";
-import { AuthUserResponse, AuthUserResponseSchema } from "../../types/auth/resp_auth_user";
-import { StdErrOrNull, WouldFail } from "../../types/error/std_error";
+import GetAuthMethod from "../../request/encapsulation/get_auth_method";
+import { AuthUserResponseSchema } from "../../types/auth/resp_auth_user";
 
 /**
  * Get the info of @param auth.
  */
-export default async function GetUserInfo(auth: CSCAuth): Promise<WouldFail<AuthUserResponse>> {
-    const rRes = await GetJsonAuth(BuildUri("/auth/user"), auth);
-
-    if (rRes && rRes.ok) {
-        const response = await rRes.json();
-
-        const successResp = AuthUserResponseSchema.try(response);
-        if (!(successResp instanceof ValidationError)) return successResp;
-
-        return StdErrOrNull(response);
-    }
-
-    return null;
+export default async function GetUserInfo(auth: CSCAuth) {
+    return GetAuthMethod("/auth/user", auth, AuthUserResponseSchema);
 }
