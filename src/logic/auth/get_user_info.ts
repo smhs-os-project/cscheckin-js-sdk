c422295cf003cc62ed193fcf730ca6aaadaa7758
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-15 11:05:28 +0800

    feat(logic/auth/get_user_info): add stub

---
+++ src/logic/auth/get_user_info.ts
@@ -0,0 +1,11 @@
+import CSCAuth from "../../auth";
+import { AuthTokenResponse } from "../../types/auth/resp_auth_token";
+
+/**
+ * Get the info of @param auth.
+ * 
+ * @returns Is it success?
+ */
+export default function GetUserInfo(auth: CSCAuth): boolean {
+    throw new Error("not implemented");
+}
