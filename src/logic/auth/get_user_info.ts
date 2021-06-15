4defacdecc3a5b85a86ea8ff63463c29399d0a6c
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-16 01:44:07 +0800

    docs(logic/auth/get_user_info): improve

--- src/logic/auth/get_user_info.ts
+++ src/logic/auth/get_user_info.ts
@@ -3,7 +3,7 @@ import GetAuthMethod from "../../request/encapsulation/get_auth_method";
 import { AuthUserResponseSchema } from "../../types/auth/resp_auth_user";
 
 /**
- * Get the info of @param auth.
+ * Get the owner info by @param auth.
  */
 export default async function GetUserInfo(auth: CSCAuth) {
     return GetAuthMethod("/auth/user", auth, AuthUserResponseSchema);
