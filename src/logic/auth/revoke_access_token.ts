3e830800c69dfb8fac799cf8d87a365a39e58cee
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-15 22:53:37 +0800

    refactor(logic/auth/revoke_access_token): DeleteJsonAuth

--- src/logic/auth/revoke_access_token.ts
+++ src/logic/auth/revoke_access_token.ts
@@ -1,11 +1,14 @@
 import CSCAuth from "../../auth";
-import { AuthTokenResponse } from "../../types/auth/resp_auth_token";
+import BuildUri from "../../request/build_uri";
+import DeleteJsonAuth from "../../request/json/delete_auth";
 
 /**
  * Revoke the access token
  * 
  * @returns Is it success?
  */
-export default function RevokeAccessToken(auth: CSCAuth): boolean {
-    throw new Error("not implemented");
+export default async function RevokeAccessToken(auth: CSCAuth): Promise<boolean> {
+    const response = await DeleteJsonAuth(BuildUri("/auth/token"), auth);
+
+    return response?.ok ?? false;
 }
