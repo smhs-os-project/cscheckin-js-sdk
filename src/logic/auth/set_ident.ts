3b90e3b5620d9af65fcdfba9901e869f579b1dfa
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-16 01:44:41 +0800

    fix(logic/auth/set_ident): forgot the request

--- src/logic/auth/set_ident.ts
+++ src/logic/auth/set_ident.ts
@@ -1,14 +1,15 @@
 import CSCAuth from "../../auth";
 import BuildUri from "../../request/build_uri";
-import GetJsonAuth from "../../request/json/get_auth";
+import PostJsonAuth from "../../request/json/post_auth";
+import { AuthIdentRequest } from "../../types/auth/req_auth_ident";
 
 /**
  * Set the identity of @param auth.
  * 
  * @returns Is it success?
  */
-export default async function SetIdentity(auth: CSCAuth): Promise<boolean> {
-    const response = await GetJsonAuth(BuildUri("/auth/user"), auth);
+export default async function SetIdentity(request: AuthIdentRequest, auth: CSCAuth): Promise<boolean> {
+    const response = await PostJsonAuth(BuildUri("/auth/user"), request, auth);
 
     return response?.ok ?? false;
 }
