817b61f5cc935b5161ff789ad6df0d31d1ee54df
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-15 11:06:06 +0800

    feat(logic/auth/set_ident): add stub

---
+++ src/logic/auth/set_ident.ts
@@ -0,0 +1,11 @@
+import CSCAuth from "../../auth";
+import { AuthTokenResponse } from "../../types/auth/resp_auth_token";
+
+/**
+ * Set the identity of @param auth.
+ * 
+ * @returns Is it success?
+ */
+export default function SetIdentity(auth: CSCAuth): boolean {
+    throw new Error("not implemented");
+}
