1fe9270f807263c8e7b5e594dbc3a5a9ebe222fe
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-16 07:46:07 +0800

    build(dist): 0.1.0-preview.3

--- dist/auth.d.ts
+++ dist/auth.d.ts
@@ -1,6 +1,12 @@
+import { Infer } from "myzod";
 import { Organization } from "./types/auth/req_auth_token";
 import { AuthTokenResponse } from "./types/auth/resp_auth_token";
 import { AuthUserResponse } from "./types/auth/resp_auth_user";
+export declare const CSCAuthExportStructure: import("myzod/libs/types").ObjectType<{
+    organization: import("myzod/libs/types").EnumType<typeof Organization>;
+    gIdToken: import("myzod/libs/types").StringType;
+    gAccessToken: import("myzod/libs/types").StringType;
+}>;
 export default class CSCAuth {
     private organization;
     private gIdToken;
@@ -11,5 +17,7 @@ export default class CSCAuth {
     getAuthenticationHeader(): Promise<string | null>;
     userInfo(): Promise<AuthUserResponse | null>;
     setIdentity(userClass: number, userNo: number): Promise<boolean>;
+    static import(data: string): CSCAuth | null;
+    export(): Infer<typeof CSCAuthExportStructure>;
 }
 //# sourceMappingURL=auth.d.ts.map
--- dist/auth.js
+++ dist/auth.js
@@ -3,10 +3,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const myzod_1 = require("myzod");
+exports.CSCAuthExportStructure = void 0;
+const myzod_1 = __importDefault(require("myzod"));
+const myzod_2 = require("myzod");
 const get_access_token_1 = __importDefault(require("./logic/auth/get_access_token"));
 const set_ident_1 = __importDefault(require("./logic/auth/set_ident"));
+const req_auth_token_1 = require("./types/auth/req_auth_token");
 const resp_auth_token_1 = require("./types/auth/resp_auth_token");
+exports.CSCAuthExportStructure = myzod_1.default.object({
+    organization: myzod_1.default.enum(req_auth_token_1.Organization),
+    gIdToken: myzod_1.default.string(),
+    gAccessToken: myzod_1.default.string(),
+});
 class CSCAuth {
     constructor(organization, gIdToken, gAccessToken) {
         this.organization = organization;
@@ -22,7 +30,7 @@ class CSCAuth {
                 access_token: this.gAccessToken
             });
             const resp = resp_auth_token_1.AuthTokenResponseSchema.try(accessData);
-            if (resp instanceof myzod_1.ValidationError) {
+            if (resp instanceof myzod_2.ValidationError) {
                 console.error("error: accessData is not a valid data.");
                 console.log(accessData);
             }
@@ -46,6 +54,20 @@ class CSCAuth {
     }
     async setIdentity(userClass, userNo) {
         return set_ident_1.default({ class: userClass, number: userNo }, this);
+    }
+    static import(data) {
+        const deserialized = exports.CSCAuthExportStructure.try(JSON.parse(data));
+        if (deserialized instanceof myzod_2.ValidationError)
+            return null;
+        const { organization, gAccessToken, gIdToken } = deserialized;
+        return new CSCAuth(organization, gAccessToken, gIdToken);
+    }
+    export() {
+        return {
+            organization: this.organization,
+            gAccessToken: this.gAccessToken,
+            gIdToken: this.gIdToken,
+        };
     }
 }
 exports.default = CSCAuth;
