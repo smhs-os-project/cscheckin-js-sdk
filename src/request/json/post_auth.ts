a59baf4d6fd65bba12829605fba64d0ba4e11f9c
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-15 11:09:20 +0800

    feat(request/json/post_auth): done

---
+++ src/request/json/post_auth.ts
@@ -0,0 +1,15 @@
+import CSCAuth from "../../auth";
+import BasePostJson from "./post_base";
+
+export default async function GetJsonAuth<T extends object>(url: RequestInfo, data: T, auth: CSCAuth): Promise<Response | null> {
+    const authentication = await auth.getAuthenticationHeader();
+
+    if (authentication)
+        return BasePostJson(url, data, {
+            headers: {
+                "Authorization": authentication,
+            },
+        });
+    
+    return null;
+}
