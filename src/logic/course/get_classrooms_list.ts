c465271c19997e411cf54ecc620851aec76b3cea
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-15 11:06:24 +0800

    feat(logic/course/get_classroom_list): add stub

---
+++ src/logic/course/get_classrooms_list.ts
@@ -0,0 +1,10 @@
+import CSCAuth from "../../auth";
+
+/**
+ * Get the available classrooms.
+ * 
+ * @returns Is it success?
+ */
+export default function GetClassroomsList(auth: CSCAuth): boolean {
+    throw new Error("not implemented");
+}
