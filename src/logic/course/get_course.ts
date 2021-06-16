083edcbf432c38b2b5045e6ef98d9a7497a91dd9
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-16 19:37:46 +0800

    feat(logic/course/get_course): GetCourse is a public API

--- src/logic/course/get_course.ts
+++ src/logic/course/get_course.ts
@@ -1,10 +1,9 @@
-import CSCAuth from "../../auth";
-import GetAuthMethod from "../../request/encapsulation/get_auth_method";
+import GetMethod from "../../request/encapsulation/get_method";
 import { CourseResponseSchema } from "../../types/course/resp_course";
 
 /**
  * Get the info of specified course.
  */
-export default async function GetCourse(courseUUID: string, auth: CSCAuth) {
-    return GetAuthMethod(`/course/${courseUUID}`, auth, CourseResponseSchema);
+export default async function GetCourse(courseUUID: string) {
+    return GetMethod(`/course/${courseUUID}`, CourseResponseSchema);
 }
