29649ed72999fd9aa22c4c55cf24bdff36b6ad4a
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-17 02:07:30 +0800

    feat(logic/course/create_course): done

--- src/logic/course/create_course.ts
+++ src/logic/course/create_course.ts
@@ -1,12 +1,20 @@
 import CSCAuth from "../../auth";
-
-export interface TimeDiff {
-    minutes: number;
-}
+import PostAuthMethod from "../../request/encapsulation/post_auth_method";
+import { CreateCourseRequest } from "../../types/course/req_course";
+import { CourseResponseSchema } from "../../types/course/resp_course";
 
 /**
  * Create a course.
+ * 
+ * @example
+ * ```ts
+ * CreateCourse("12345", {
+ *   start_timestamp: new Date(),
+ *   late_time: "00:10:00",
+ *   expire_time: "00:50:00",
+ * });
+ * ```
  */
-export default function CreateCourse(classroomId: string, start: Date, late: TimeDiff, end: TimeDiff) {
-    throw new Error("not implemented");
+export default function CreateCourse(classroomId: string, request: CreateCourseRequest, auth: CSCAuth) {
+    return PostAuthMethod(`/course/${classroomId}`, request, auth, CourseResponseSchema);
 }
