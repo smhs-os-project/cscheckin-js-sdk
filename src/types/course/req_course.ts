d6c45f71346c0c131e52393b368914649d6ce356
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-17 02:07:44 +0800

    feat(types/course/req_course): more details readme

--- src/types/course/req_course.ts
+++ src/types/course/req_course.ts
@@ -3,7 +3,13 @@ import { timePlusPattern } from "../../utilities/is_after";
 
 export const CreateCourseRequestSchema = myzod.object({
     start_timestamp: myzod.date(),
+    /**
+     * Set students checking in after "hh:mm:ss" to "late".
+     */
     late_time: myzod.string().pattern(timePlusPattern),
+    /**
+     * Expire the course after "hh:mm:ss"
+     */
     expire_time: myzod.string().pattern(timePlusPattern),
 });
 
