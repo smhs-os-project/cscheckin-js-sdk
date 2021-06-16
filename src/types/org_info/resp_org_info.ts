48de03576451b11428b72cc58035cc02e33a7b7a
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-16 08:05:24 +0800

    feat(types/org_info/resp_org_info): id

--- src/types/org_info/resp_org_info.ts
+++ src/types/org_info/resp_org_info.ts
@@ -1,11 +1,17 @@
 import myzod, { Infer }  from "myzod";
+import { Organization } from "../auth/req_auth_token";
 
 export const OrgInfoResponseSchema = myzod.object({
     client_id: myzod.string(),
     chinese_name: myzod.string(),
 });
 
-export const OrgInfoListResponseSchema = myzod.array(OrgInfoResponseSchema);
+export const OrgInfoListResponseSchema = myzod.array(
+    OrgInfoResponseSchema
+        .and(myzod.object({
+            id: myzod.enum(Organization),
+        }))
+);
 
 export type OrgInfoResponse = Infer<typeof OrgInfoResponseSchema>;
 export type OrgInfoListResponse = Infer<typeof OrgInfoListResponseSchema>;
