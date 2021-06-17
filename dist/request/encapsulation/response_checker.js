40a75a0660ccb0db0aeb968a58307838355365aa
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-17 13:10:18 +0800

    build(dist): 0.1.0-preview.8a

--- dist/request/encapsulation/response_checker.js
+++ dist/request/encapsulation/response_checker.js
@@ -9,6 +9,8 @@ async function ResponseChecker(response, schema) {
             const successResp = schema.try(responseJson);
             if (!(successResp instanceof myzod_1.ValidationError))
                 return successResp;
+            console.error("received a malformed response.");
+            console.error(responseJson);
             return std_error_1.StdErrOrNull(responseJson);
         }
         console.error(`response failed: ${response.status}\ndetails: ${await response.text()}`);
