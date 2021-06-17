18919e6103f7b1d3d225d298803b005c137ac885
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-17 13:50:01 +0800

    build(dist): 0.1.0-preview.8c

--- dist/types/common/checkin_state.js
+++ dist/types/common/checkin_state.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.CheckinState = void 0;
 var CheckinState;
 (function (CheckinState) {
-    CheckinState["NOT_CHECK_IN"] = "NOT_CHECK_IN";
+    CheckinState["NOT_CHECKED_IN"] = "NOT_CHECKED_IN";
     CheckinState["ON_TIME"] = "ON_TIME";
     CheckinState["LATE"] = "LATE";
 })(CheckinState = exports.CheckinState || (exports.CheckinState = {}));
