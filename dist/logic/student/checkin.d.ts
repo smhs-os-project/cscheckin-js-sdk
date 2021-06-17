e25598a3d8173d55b52e4a42b04f7145a8711527
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-17 10:46:12 +0800

    build(dist): 0.1.0-preview.7d

--- dist/logic/student/checkin.d.ts
+++ dist/logic/student/checkin.d.ts
@@ -1,5 +1,5 @@
 import type CSCAuth from "../../auth";
-export default function Checkin(checkinId: number, auth: CSCAuth): Promise<import("../../types/error/std_error").WouldFail<{
+export default function Checkin(courseUUID: string, auth: CSCAuth): Promise<import("../../types/error/std_error").WouldFail<{
     id: number;
     created_at: Date;
     updated_at: Date;
--- dist/logic/student/checkin.js
+++ dist/logic/student/checkin.js
@@ -5,8 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const post_auth_method_1 = __importDefault(require("../../request/encapsulation/post_auth_method"));
 const resp_checkin_1 = require("../../types/student/resp_checkin");
-async function Checkin(checkinId, auth) {
-    return post_auth_method_1.default(`/checkin/${checkinId}`, {}, auth, resp_checkin_1.StudentCheckinResponseSchema);
+async function Checkin(courseUUID, auth) {
+    return post_auth_method_1.default(`/checkin/${courseUUID}`, {}, auth, resp_checkin_1.StudentCheckinResponseSchema);
 }
 exports.default = Checkin;
 //# sourceMappingURL=checkin.js.map
