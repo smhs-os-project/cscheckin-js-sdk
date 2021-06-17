b0efa70953e0f321ef2f3ebdf3c64bba1f75e795
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-17 10:43:16 +0800

    fix(logic/student/checkin): REVERT checkinId should be number
    This reverts commit e339feeda2f98659e7c26c97bf9200e8169fbda8.

--- src/logic/student/checkin.ts
+++ src/logic/student/checkin.ts
@@ -2,6 +2,6 @@ import type CSCAuth from "../../auth";
 import PostAuthMethod from "../../request/encapsulation/post_auth_method";
 import { StudentCheckinResponseSchema } from "../../types/student/resp_checkin";
 
-export default async function Checkin(checkinId: number, auth: CSCAuth) {
+export default async function Checkin(checkinId: string, auth: CSCAuth) {
     return PostAuthMethod(`/checkin/${checkinId}`, {}, auth, StudentCheckinResponseSchema);
 }
