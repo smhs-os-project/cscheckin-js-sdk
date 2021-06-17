d9ea97271bc82a7b123bad7c050b7abb8729543e
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-17 12:45:18 +0800

    feat(logic/course/sync_course_members): POST /course/sync/{courseId}

---
+++ src/logic/course/sync_course_members.ts
@@ -0,0 +1,12 @@
+import CSCAuth from "../../auth";
+import BuildUri from "../../request/build_uri";
+import PostJsonAuth from "../../request/json/post_auth";
+
+/**
+ * Sync the members list of the specified course.
+ */
+export default async function SyncCourseMembers(courseId: number, auth: CSCAuth) {
+    const response = await PostJsonAuth(BuildUri(`/course/sync/${courseId}`), {}, auth);
+
+    return response?.ok ?? false;
+}
