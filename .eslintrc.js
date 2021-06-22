c7ff058e63b4f35d3ed6e8094f569ac5aa7c1773
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-22 23:32:51 +0800

    feat(lint/eslint): configure

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    'airbnb-typescript/base',
+    "plugin:prettier/recommended",
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+  },
+};
