5533b69521786cbcc7dca80880105e9dd3629208
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-28 00:17:00 +0800

    refactor!(auth): dropped Organization-related stuff

--- src/auth.ts
+++ src/auth.ts
@@ -5,14 +5,10 @@ import GetAccessToken from "./logic/auth/get_access_token";
 import GetUserInfo from "./logic/auth/get_user_info";
 import RevokeAccessToken from "./logic/auth/revoke_access_token";
 import SetIdentity from "./logic/auth/set_ident";
-import { Organization } from "./types/auth/req_auth_token";
-import type { AuthTokenResponse } from "./types/auth/resp_auth_token";
-import { AuthTokenResponseSchema } from "./types/auth/resp_auth_token";
-import type { AuthUserResponse } from "./types/auth/resp_auth_user";
-import { AuthUserResponseSchema } from "./types/auth/resp_auth_user";
+import type { AuthTokenResponse, AuthUserResponse } from "./types";
+import { AuthTokenResponseSchema, AuthUserResponseSchema } from "./types";
 
 export const CSCAuthExportStructure = myzod.object({
-  organization: myzod.enum(Organization),
   gIdToken: myzod.string(),
   gAccessToken: myzod.string(),
   accessData: AuthTokenResponseSchema.optional(),
@@ -21,16 +17,12 @@ export const CSCAuthExportStructure = myzod.object({
 export default class CSCAuth {
   private accessData: AuthTokenResponse | null = null;
 
-  constructor(
-    private organization: Organization,
-    private gIdToken: string,
-    private gAccessToken: string
-  ) {}
+  constructor(private gIdToken: string, private gAccessToken: string) {}
 
   async getAccessData(): Promise<AuthTokenResponse | null> {
     // get lazily
     if (!this.accessData) {
-      const accessData = await GetAccessToken(this.organization, {
+      const accessData = await GetAccessToken({
         id_token: this.gIdToken,
         access_token: this.gAccessToken,
       });
@@ -72,11 +64,11 @@ export default class CSCAuth {
     return null;
   }
 
-  async setIdentity(userClass: string, userNo: string): Promise<boolean> {
+  async setIdentity(userClass: string, userNo: string) {
     return SetIdentity({ class: userClass, number: userNo }, this);
   }
 
-  async revoke(): Promise<boolean> {
+  async revoke() {
     return RevokeAccessToken(this);
   }
 
@@ -84,8 +76,8 @@ export default class CSCAuth {
     const deserialized = CSCAuthExportStructure.try(JSON.parse(data));
     if (deserialized instanceof ValidationError) return null;
 
-    const { organization, gAccessToken, gIdToken, accessData } = deserialized;
-    const auth = new CSCAuth(organization, gAccessToken, gIdToken);
+    const { gAccessToken, gIdToken, accessData } = deserialized;
+    const auth = new CSCAuth(gAccessToken, gIdToken);
     auth.accessData = accessData || null;
 
     return auth;
@@ -93,7 +85,6 @@ export default class CSCAuth {
 
   export(): string {
     return JSON.stringify({
-      organization: this.organization,
       gAccessToken: this.gAccessToken,
       gIdToken: this.gIdToken,
       accessData: this.accessData,
