bbb2c91cb7dd1e83a2e9e1fca92d22cfee7af197
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-29 23:51:22 +0800

    fix(logic/student/checkin): it returns JSON

--- src/logic/student/checkin.ts
+++ src/logic/student/checkin.ts
@@ -1,11 +1,12 @@
 import type CSCAuth from "../../auth";
 import Client, { clientInstance } from "../../request/client";
+import { CheckinResponseSchema } from "../../types/student/resp_checkin";
 
 export default async function Checkin(courseUUID: string, auth: CSCAuth) {
-  const { statusCode } = await clientInstance.jsonFetcher(
+  const { data } = await clientInstance.jsonFetcher(
     `/checkin/${courseUUID}`,
     Client.postJsonRequest({}, await Client.authRequest(auth))
   );
 
-  return Client.isResponseOk(statusCode ?? -1);
+  return Client.responseParser(data, CheckinResponseSchema);
 }
